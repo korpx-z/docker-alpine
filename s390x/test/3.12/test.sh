8ec5b9b7b5d272e0e39a8e112142d578a2b30d32
charles-korpics <charles.korpics@ibm.com>
2020-10-25 16:12:49 -0400

    testing

---
+++ s390x/test/3.12/test.sh
@@ -0,0 +1,14 @@
+set -e
+
+export ANSI_YELLOW="\e[1;33m"
+export ANSI_GREEN="\e[32m"
+export ANSI_RESET="\e[0m"
+
+echo -e "\n $ANSI_YELLOW *** testing docker run - alpine *** $ANSI_RESET \n"
+
+echo -e "$ANSI_YELLOW Display current bash version: $ANSI_RESET"
+docker build . --tag test/quay.io/ibmz/alpine:3.12
+docker run --rm --name some-alpine test/quay.io/ibmz/alpine3:12 bash --version
+docker rmi test/quay.io/ibmz/alpine3:12
+
+echo -e "\n $ANSI_GREEN *** TEST COMPLETED SUCESSFULLY *** $ANSI_RESET \n"
