a2c3f6a16697a95ac68dad77c5c0a9ccdb18e7b9
korpx-z <72992136+korpx-z@users.noreply.github.com>
2020-10-25 16:17:15 -0400

    Update test.sh

--- s390x/test/3.12/test.sh
+++ s390x/test/3.12/test.sh
@@ -7,8 +7,8 @@ export ANSI_RESET="\e[0m"
 echo -e "\n $ANSI_YELLOW *** testing docker run - alpine *** $ANSI_RESET \n"
 
 echo -e "$ANSI_YELLOW Display current bash version: $ANSI_RESET"
-docker build . --tag test/quay.io/ibmz/alpine:3.12
-docker run --rm --name some-alpine test/quay.io/ibmz/alpine3:12 --version
-docker rmi test/quay.io/ibmz/alpine3:12
+docker build . --tag local/alpine:3.12
+docker run --rm --name some-alpine local/alpine:3.12 --version
+docker rmi local/alpine:3.12
 
 echo -e "\n $ANSI_GREEN *** TEST COMPLETED SUCESSFULLY *** $ANSI_RESET \n"
