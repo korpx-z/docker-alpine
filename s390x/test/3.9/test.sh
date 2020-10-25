5fc0b3cfd536637658db0778c962b3a7365b4fcf
charles-korpics <charles.korpics@ibm.com>
2020-10-25 16:42:20 -0400

    image tag

--- s390x/test/3.9/test.sh
+++ s390x/test/3.9/test.sh
@@ -8,7 +8,7 @@ echo -e "\n $ANSI_YELLOW *** testing docker run - alpine *** $ANSI_RESET \n"
 
 echo -e "$ANSI_YELLOW Display current directory: $ANSI_RESET"
 docker build . --tag local/alpine:3.9
-docker run --rm --name some-alpine local/alpine:3.12 pwd
+docker run --rm --name some-alpine local/alpine:3.9 pwd
 docker rmi local/alpine:3.9
 
 echo -e "\n $ANSI_GREEN *** TEST COMPLETED SUCESSFULLY *** $ANSI_RESET \n"
