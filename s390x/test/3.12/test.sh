93e750105c80f7bc208501acefe5de62af9faa8e
Charles Korpics <72992136+korpx-z@users.noreply.github.com>
2020-10-29 11:46:22 -0400

    Update test.sh

--- s390x/test/3.12/test.sh
+++ s390x/test/3.12/test.sh
@@ -8,7 +8,7 @@ echo -e "\n $ANSI_YELLOW *** testing docker run - alpine *** $ANSI_RESET \n"
 
 echo -e "$ANSI_YELLOW Display current directory: $ANSI_RESET"
 docker build . --tag local/alpine:3.12
-docker run --rm --name some-alpine local/alpine:3.12 pwd
+docker run --rm --name some-alpine local/alpine:3.12 echo 'hello'
 docker rmi local/alpine:3.12
 
 echo -e "\n $ANSI_GREEN *** TEST COMPLETED SUCESSFULLY *** $ANSI_RESET \n"
